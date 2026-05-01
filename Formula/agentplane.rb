c6d3e9435c729dfe46ebf933333da6d12bd23f57
Denis Smirnov <densmirnov@me.com>
2026-05-02 04:46:15 +0700

    agentplane: fix 0.4.1 npm install

--- Formula/agentplane.rb
+++ Formula/agentplane.rb
@@ -3,13 +3,16 @@ class Agentplane < Formula
   homepage "https://agentplane.org"
   url "https://registry.npmjs.org/agentplane/-/agentplane-0.4.1.tgz"
   sha256 "76edd130dceddb1d15313a5feb3819c513c815b350b9abc822b3ea4712ccc74b"
+  version "0.4.1"
   license "MIT"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink libexec.glob("bin/*")
+    system "npm", "install", "--global", "--prefix", libexec,
+      "--omit=dev", "--ignore-scripts", "--no-audit", "--no-fund",
+      cached_download
+    bin.install_symlink Dir[libexec/"bin/*"]
   end
 
   test do
